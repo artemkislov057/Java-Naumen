ea86864376bed3335fddf7b5f19353005b155b16
a.kislov <a.kislov@skbkontur.ru>
2023-04-06 18:51:03 +0500

    Add empty java spring project

---
+++ bookingStand/src/test/java/urfu/bookingStand/BookingStandApplicationTests.java
@@ -0,0 +1,13 @@
+package urfu.bookingStand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingStandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
