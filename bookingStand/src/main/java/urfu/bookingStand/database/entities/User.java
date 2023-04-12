3e6dffc6674f382599f813500764479f6d81092c
a.kislov <a.kislov@skbkontur.ru>
2023-04-12 18:12:53 +0500

    Change tables naming to singular in user_role

--- bookingStand/src/main/java/urfu/bookingStand/database/entities/User.java
+++ bookingStand/src/main/java/urfu/bookingStand/database/entities/User.java
@@ -23,7 +23,7 @@ public class User {
     private String password;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.ORDINAL)
     private Set<Role> roles;
 
