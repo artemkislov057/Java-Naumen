589e3b0804d5aa6e0b448695ecf9f7efa27f281a
SweetSnowyWitch <snowysatan@yandex.ru>
2023-05-11 14:09:33 +0500

    Add Teams Getting By Id, Name And Description

--- bookingStand/src/main/java/urfu/bookingStand/domain/abstractions/TeamService.java
+++ bookingStand/src/main/java/urfu/bookingStand/domain/abstractions/TeamService.java
@@ -1,10 +1,19 @@
 package urfu.bookingStand.domain.abstractions;
 
-import urfu.bookingStand.domain.exceptions.NoAccessException;
+import urfu.bookingStand.api.dto.team.TeamByDescriptionDto;
+import urfu.bookingStand.api.dto.team.TeamByNameDto;
+import urfu.bookingStand.api.dto.team.TeamByTeamIdDto;
 import urfu.bookingStand.domain.requests.AddTeamRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TeamService {
     void AddTeam(AddTeamRequest request);
+
+    List<TeamByTeamIdDto> getTeamsByTeamId(UUID teamId);
+
+    List<TeamByNameDto> getTeamsByName(String name);
+
+    List<TeamByDescriptionDto> getTeamsByDescription(String description);
 }
--- bookingStand/src/main/java/urfu/bookingStand/domain/services/TeamServiceImpl.java
+++ bookingStand/src/main/java/urfu/bookingStand/domain/services/TeamServiceImpl.java
@@ -3,17 +3,16 @@ package urfu.bookingStand.domain.services;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import urfu.bookingStand.database.entities.Stand;
+import urfu.bookingStand.api.dto.team.TeamByDescriptionDto;
+import urfu.bookingStand.api.dto.team.TeamByNameDto;
+import urfu.bookingStand.api.dto.team.TeamByTeamIdDto;
 import urfu.bookingStand.database.entities.Team;
-import urfu.bookingStand.database.repositories.StandRepository;
 import urfu.bookingStand.database.repositories.TeamRepository;
-import urfu.bookingStand.database.repositories.UserRepository;
-import urfu.bookingStand.database.repositories.UserTeamAccessRepository;
 import urfu.bookingStand.domain.abstractions.TeamService;
-import urfu.bookingStand.domain.exceptions.NoAccessException;
 import urfu.bookingStand.domain.requests.AddTeamRequest;
 
-import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -34,5 +33,44 @@ public class TeamServiceImpl implements TeamService {
         team.setName(request.getName());
         team.setDescription(request.getDescription());
         teamRepository.save(team);
+    }
+
+    @Override
+    public List<TeamByTeamIdDto> getTeamsByTeamId(UUID teamId) {
+        var teamsByTeamIdDto = new ArrayList<TeamByTeamIdDto>();
+
+        for (var team : teamRepository.findAll()) {
+            if (team.getId().equals(teamId)) {
+                var teamDto = modelMapper.map(team, TeamByTeamIdDto.class);
+                teamsByTeamIdDto.add(teamDto);
+            }
+        }
+        return teamsByTeamIdDto;
+    }
+
+    @Override
+    public List<TeamByNameDto> getTeamsByName(String name) {
+        var teamsByNameDto = new ArrayList<TeamByNameDto>();
+
+        for (var team : teamRepository.findAll()) {
+            if (team.getName().equals(name)) {
+                var teamDto = modelMapper.map(team, TeamByNameDto.class);
+                teamsByNameDto.add(teamDto);
+            }
+        }
+        return teamsByNameDto;
+    }
+
+    @Override
+    public List<TeamByDescriptionDto> getTeamsByDescription(String description) {
+        var teamsByDescriptionDto = new ArrayList<TeamByDescriptionDto>();
+
+        for (var team : teamRepository.findAll()) {
+            if (team.getDescription().equals(description)) {
+                var teamDto = modelMapper.map(team, TeamByDescriptionDto.class);
+                teamsByDescriptionDto.add(teamDto);
+            }
+        }
+        return teamsByDescriptionDto;
     }
 }
