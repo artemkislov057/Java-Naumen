60ada7f89004d2247b73f61c9dadd7f627ee76c7
a.kislov <a.kislov@skbkontur.ru>
2023-05-11 14:19:27 +0500

    Delete extra controller

--- bookingStand/src/main/java/urfu/bookingStand/database/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package urfu.bookingStand.database.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import urfu.bookingStand.database.entities.Book;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
