89a32e6287d6740fbf62803e271d3b671ba2d441
Alexey Kucherenko <82213188+RenKOFFF@users.noreply.github.com>
2023-05-11 17:42:28 +0500

    new file

--- bookingStand/src/main/java/urfu/bookingStand/api/mapperConfigs/StandEmploymentDtoConfig.java
+++ bookingStand/src/main/java/urfu/bookingStand/api/mapperConfigs/StandEmploymentDtoConfig.java
@@ -4,16 +4,19 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import urfu.bookingStand.api.dto.stand.StandEmploymentDto;
 import urfu.bookingStand.api.dto.user.RegisterUserDto;
 import urfu.bookingStand.database.entities.Booking;
 import urfu.bookingStand.domain.requests.RegisterUserRequest;
 import urfu.bookingStand.domain.responses.StandEmploymentResponse;
 
 @Configuration
-public class RegisterUserDtoConfig {
+public class StandEmploymentDtoConfig {
     @Autowired
     public void ConfigureMap(ModelMapper mapper) {
-        var map = mapper.createTypeMap(RegisterUserDto.class, RegisterUserRequest.class);
-        map.addMapping(RegisterUserDto::getLogin, RegisterUserRequest::setShortName);
+        mapper.addMappings(new PropertyMap<Booking, StandEmploymentResponse>() {
+            protected void configure() {
+                map().getUser().setUserId(source.getUser().getId());
+            }});
     }
 }
